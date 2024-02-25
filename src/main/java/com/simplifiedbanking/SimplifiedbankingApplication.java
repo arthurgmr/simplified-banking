0afcf8e9978bc2034d7535b8b942d483fdaada66
Arthur Machado <arthur.machado@maxmilhas.com.br>
2024-02-25 11:38:27 -0300

    first commit

---
+++ src/main/java/com/simplifiedbanking/SimplifiedbankingApplication.java
@@ -0,0 +1,13 @@
+package com.simplifiedbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedbankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplifiedbanking/SimplifiedbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifiedbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
