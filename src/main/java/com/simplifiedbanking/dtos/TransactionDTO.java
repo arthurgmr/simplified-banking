82c07ebf5ef71e4d1524efc58d47d61fadc97f49
Arthur Machado <arthur.machado@maxmilhas.com.br>
2024-02-27 21:46:41 -0300

    feat: add transaction service

---
+++ src/main/java/com/simplifiedbanking/dtos/TransactionDTO.java
@@ -0,0 +1,8 @@
+package com.simplifiedbanking.dtos;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record TransactionDTO(BigDecimal value, UUID senderId, UUID receiverId) {
+  
+}
